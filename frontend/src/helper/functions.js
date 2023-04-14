28227ec31fce78019dbdda6bb9f9f698bf3837d0
Ujjwal Garg <ujjwalgarg.252@gmail.com>
2023-04-14 06:13:46 +0530

    time console addded

--- frontend/src/helper/functions.js
+++ frontend/src/helper/functions.js
@@ -3,6 +3,7 @@ import moment from "moment";
 export const checkisTimeBetween = (start, end) => {
   let forma = "HH:mm";
   const time = moment();
+  console.log("time", time);
   const beforeTime = moment(moment(start).format(forma), forma);
   const afterTime = moment(moment(end).format(forma), forma);
 
